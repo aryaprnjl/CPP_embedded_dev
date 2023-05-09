74ba8a99e452d2389e9d2382ec1770f46c993a02
Pranjal Arya <pranjalarya9999@gmail.com>
2023-05-09 12:14:08 +0530

    Default arguments in function
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>

--- cpp_applications/ref_var/app.cpp
+++ cpp_applications/ref_var/app.cpp
@@ -1,23 +1,25 @@
+// Notes: 1,2,3,4,5,6,7,8,9
+
 int first()
 {
     int x=0;
-	int x1=1;
-	int &y=x;
-	
-	// ref var should be initialized with declaration
-	//int &z;
-	
-	// ref var can't be reinitialized
-	//&y=x1;
+    int x1=1;
+    int &y=x;
 
-	y=x1;
+    // ref var should be initialized with declaration
+    //int &z;
 
-    return y;  
+    // ref var can't be reinitialized
+    //&y=x1;
+
+    y=x1;
+
+    return y;
 }
 extern "C"
 {
-	int main()
-	{
-		return first();
-	}
+    int main()
+    {
+        return first();
+    }
 }
