5cf7d5107e844610bed2b9459efeb744adfe2256
Pranjal Arya <pranjalarya9999@gmail.com>
2023-05-09 21:35:27 +0530

    Default arguments in function (#9)
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -18,39 +18,41 @@ fi
 
 get_dep()
 {
-	if [[ ! -d $ROOT_FOLDER/stm_files ]]; then
-		unzip $ROOT_FOLDER/stm_files.zip -d $ROOT_FOLDER/stm_files
-		if [[ -d $ROOT_FOLDER/stm_files/C: ]]; then
-			mv $ROOT_FOLDER/stm_files/"$(dirname $ROOT_FOLDER)"/CPP_embedded_dev/stm_files/* $ROOT_FOLDER/stm_files/
-			rm -rf $ROOT_FOLDER/stm_files/C:
-		fi
-		rm -rf $ROOT_FOLDER/C:
-	fi
+    if [[ ! -d $ROOT_FOLDER/stm_files ]]; then
+        unzip $ROOT_FOLDER/stm_files.zip -d $ROOT_FOLDER/stm_files
+        if [[ -d $ROOT_FOLDER/stm_files/C: ]]; then
+            mv $ROOT_FOLDER/stm_files/"$(dirname $ROOT_FOLDER)"/CPP_embedded_dev/stm_files/* $ROOT_FOLDER/stm_files/
+            rm -rf $ROOT_FOLDER/stm_files/C:
+        fi
+        rm -rf $ROOT_FOLDER/C:
+    fi
 }
 
 if [[ $1 == build ]];then
-	get_dep
+    get_dep
     make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI all
 elif [[ $1 == erase ]];then
     make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI erase
 elif [[ $1 == flash ]];then
-	get_dep
+    get_dep
     make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI flash
 elif [[ $1 == debug ]];then
-	get_dep
-	if [[ ! -f $ROOT_FOLDER/cpp_applications/build/$2/$2.elf ]]; then
-		make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI all
-	fi
-    openocd -s scripts -f $ROOT_FOLDER/build_scripts/stm.cfg -c "init; reset init" &
-    sleep 1
-    arm-none-eabi-gdb -ex "target remote :3333" \
-    -ex "monitor reset init" \
-    -ex "set confirm off" \
-    -ex "file $ROOT_FOLDER/cpp_applications/build/$2/$2.elf" \
-    -ex "load"
+    get_dep
+    if [[ ! -f $ROOT_FOLDER/cpp_applications/build/$2/$2.elf ]]; then
+        make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI all
+    fi
+    if [[ -f $ROOT_FOLDER/cpp_applications/build/$2/$2.elf ]]; then
+        openocd -s scripts -f $ROOT_FOLDER/build_scripts/stm.cfg -c "init; reset init" &
+        sleep 1
+        arm-none-eabi-gdb -ex "target remote :3333" \
+        -ex "monitor reset init" \
+        -ex "set confirm off" \
+        -ex "file $ROOT_FOLDER/cpp_applications/build/$2/$2.elf" \
+        -ex "load"
+    fi
 #    -ex "source -v $ROOT_FOLDER/build_scripts/gdb_cmd"
 elif [[ $1 == disass ]];then
-	get_dep
+    get_dep
     make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI FUNC=$3 disass
 elif [[ $1 == clean ]];then
     find . -name '*.o' -exec rm -r {} \;
--- cpp_applications/random_num_gen/app.cpp
+++ cpp_applications/random_num_gen/app.cpp
@@ -1,17 +1,19 @@
+// Notes: NULL
+
 #include <cstdlib>
-class Student {  
-   public:  
+class Student {
+   public:
       int id;
-};  
+};
 int first() {
     Student s1;
     s1.id = rand();
-    return s1.id;  
+    return s1.id;
 }
 extern "C"
 {
-	int main()
-	{
-		return first();
-	}
+    int main()
+    {
+        return first();
+    }
 }
