5d00f72ab6e95be11451eff73941c7deb768d0e9
Pranjal Arya <pranjalarya9999@gmail.com>
2023-05-12 22:29:43 +0530

    Structure (#15)
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>

--- cpp_applications/structure/app.cpp
+++ cpp_applications/structure/app.cpp
@@ -1,4 +1,4 @@
-// Notes: 14,15
+// Notes: 14,15,16,17
 
 #include <cstring>
 
@@ -6,12 +6,37 @@ typedef struct book{
     int   bookid;
     char  title[20];
     float price;
+private:
+    int prv_member;
+
+public:
+    // we can't take argument as book b. We can directly access structure members bookid, not like b.bookid.
+    void getdata(int a)
+    {
+        bookid = a;
+    }
+
+    void set_prv_member(int a)
+    {
+        prv_member = a;
+    }
 }book;
+
+book input()
+{
+    book b;
+
+    b.bookid = 200;
+    strcpy(b.title,"C++_1");
+    b.price = 400;
+
+    return b;
+}
 
 int first()
 {
-    // Can be intialized at declaration
-    book b1 = {100,"C++ book",200};
+    // Can be intialized at declaration, but this method woll work only if all members are public
+    // book b1 = {100,"C++ book",200};
 
     // Or can be initialized later
     book b2;
@@ -23,9 +48,17 @@ int first()
     // We can do this
     strcpy(b2.title,"C++");
 
-    book b3;
+    book b3,b4,b5;
     b3.bookid = b2.bookid;
     b3=b2;
+
+    b4 = input();
+
+    b5.getdata(300);
+
+    // We can't set private members directly. We need to use member functions to access those.
+    // b5.prv_member=1;
+    b5.set_prv_member(1);
 
     return 1;
 }
